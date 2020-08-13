f96dff2d975ff9380ddb1f96b880fc8ff6f27de5
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-08-13 17:50:00 -0400

    added user model and tested with seed data

--- charleston_app_api/db/schema.rb
+++ charleston_app_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_145420) do
+ActiveRecord::Schema.define(version: 2020_08_13_214550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_08_13_145420) do
     t.integer "exits"
     t.float "lat"
     t.float "long"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
