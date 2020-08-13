2f8efd827992cb5c57c33cea205e1936cc2019c7
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-08-13 11:17:18 -0400

    added all market seed data

--- charleston_app_api/db/seeds.rb
+++ charleston_app_api/db/seeds.rb
@@ -10,4 +10,14 @@
 Market.create([
     { name: 'Sea Island Farmers Market', address: '2024 Academy Road, Johns Island, SC 29455', exits: 4214, lat: 32.715936, long: -80.050307 },
     { name: 'James Island Farmers Market', address: ' 1977 Maybank Highway, Charleston, SC 29412', exits: 4214, lat: 32.762181, long: -79.987000 },
+    { name: 'West Ashley Farmers Market', address: 'Ackerman Park, 55 Sycamore Ave., Charleston 29407', exits: 4214, lat: 32.790280, long: -79.988532 },
+    { name: 'Downtown Charleston Farmers Market', address: 'Marion Square – 329 Meeting Street, Charleston 29403', exits: 4214, lat: 32.786935, long: -79.935730 },
+    { name: 'North Charleston Farmers Market', address: 'Felix C Davis Community Center, 4800 Park Circle, North Charleston 29405', exits: 4214, lat: 32.881632, long: -79.984722 },
+    { name: 'Summerville Farmers Market', address: '200 S. Main St., Summerville 29483', exits: 4214, lat: 33.018975, long: -80.176445 },
+    { name: 'Mt. Pleasant Farmers Market', address: 'Mt. Pleasant Farmers Market Pavilion (Moultrie Middle School) 645 Coleman Blvd, Mt Pleasant 29464', exits: 4214, lat: 32.790978, long: -79.872204 },
+    { name: "Sullivan's Island Farmers Market", address: 'J. Marshall Stith Park (2050 Middle Street), Sullivan’s Island, 29482', exits: 4214, lat: 32.762023, long: -79.838904 },
+    { name: "Folly Beach Farmers Market", address: 'Folly River Park, Center Street, Folly Beach 29439', exits: 4214, lat: 32.658537, long: -79.941945 },
+    { name: "Isle of Palms Farmers Market", address: '114th Avenue, Isle of Palms 29451', exits: 4214, lat: 32.787069, long: -79.786046 },
+    { name: "Moncks Corner Farmers Market", address: '418 East Main St., Moncks Corner 29461', exits: 4214, lat: 33.194818, long: -80.002113 },
+
   ])
