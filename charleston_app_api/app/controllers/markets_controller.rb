c631b37e2d3dd633310d0632929e1deb70c86f73
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-08-13 17:18:50 -0400

    edited market routes and controller to just get route

--- charleston_app_api/app/controllers/markets_controller.rb
+++ charleston_app_api/app/controllers/markets_controller.rb
@@ -1,5 +1,4 @@
 class MarketsController < ApplicationController
-  before_action :set_market, only: [:show, :update, :destroy]
 
   # GET /markets
   def index
@@ -7,45 +6,5 @@ class MarketsController < ApplicationController
 
     render json: @markets
   end
-
-  # GET /markets/1
-  def show
-    render json: @market
-  end
-
-  # POST /markets
-  def create
-    @market = Market.new(market_params)
-
-    if @market.save
-      render json: @market, status: :created, location: @market
-    else
-      render json: @market.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /markets/1
-  def update
-    if @market.update(market_params)
-      render json: @market
-    else
-      render json: @market.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /markets/1
-  def destroy
-    @market.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_market
-      @market = Market.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def market_params
-      params.require(:market).permit(:name, :address, :exits, :lat, :long)
-    end
+  
 end
