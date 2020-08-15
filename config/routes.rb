0bf0cef2dcc9062abd82b35ca0e5c30e48a680dc
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-08-15 10:55:04 -0400

    added welcome controller/route for heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   resources :users
-  resources :markets, only: [:index]
   get 'users(/username/:username)', to: 'users#display'
+  resources :markets, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
