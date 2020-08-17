cc0dd71b5f8330bdaa65c4a1584efeb958db0365
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-08-17 12:49:35 -0400

    favorite routes nested within user routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     # [prevents duplicates of any username and requires it]
+    has_many :favorites
     validates :username, :presence => true, :uniqueness => true
     validates :email, :presence => true
-    has_many :favorites
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :favorites
   resources :produces
   root 'welcome#index'
-  resources :users
+  resources :users do 
+    resources :favorites
+  end
+  # custom route, get by username
   get 'users(/username/:username)', to: 'users#display'
   resources :markets, only: [:index]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
